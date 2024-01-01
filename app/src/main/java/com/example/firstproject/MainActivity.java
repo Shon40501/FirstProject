7427aad0851f1af05ed5f2723443ad1d292f2e89
Magshimim_AI <shon40501@gmail.com>
2024-01-01 23:49:23 +0200

    my project

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -3,26 +3,29 @@ package com.example.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
+    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv=findViewById(R.id.Textview2);
-        tv.setTextSize(75);
-        tv.setText("Hi");
-        if(tv.getText() == "Hi"){
-            tv.setTextColor(Color.BLACK);
-        }
-        else{
-            tv.setTextColor(Color.BLUE);
-        }
-
-
+        bt = (Button) findViewById(R.id.button);
+        tv.setTextSize(15);
+        tv.setText("Press the butoon if u like ice cream");
+        bt.setText("Press");
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                bt.setText("good job");
+            }
+        });
 
 
     }
